b26090505258fe7d7154d331501cf690b14440b3
Muhamad Istiady Kartadibrata <mkartadibrata@gmail.com>
2021-04-14 13:14:34 +0700

    fixed bug

--- app/src/main/java/com/dicoding/githublistapplication/db/DatabaseHelper.kt
+++ app/src/main/java/com/dicoding/githublistapplication/db/DatabaseHelper.kt
@@ -11,14 +11,14 @@ internal class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATA
         private const val DATABASE_VERSION = 1
         private const val SQL_CREATE_TABLE_NOTE = "CREATE TABLE $TABLE_NAME" +
                 " (${DatabaseContract.FavoriteColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
-                " ${DatabaseContract.FavoriteColumns.NAME} TEXT NOT NULL," +
+                " ${DatabaseContract.FavoriteColumns.NAME} TEXT NULL," +
                 " ${DatabaseContract.FavoriteColumns.FOLLOWERS_URL} TEXT NULL," +
                 " ${DatabaseContract.FavoriteColumns.FOLLOWING_URL} TEXT NULL,"+
                 " ${DatabaseContract.FavoriteColumns.FOLLOWING} INTEGER NULL,"+
                 " ${DatabaseContract.FavoriteColumns.FOLLOWERS} INTEGER NULL,"+
                 " ${DatabaseContract.FavoriteColumns.AVATAR_URL} TEXT NULL,"+
                 " ${DatabaseContract.FavoriteColumns.LOGIN} TEXT NULL,"+
-                " ${DatabaseContract.FavoriteColumns.FAVORITE} TEXT NOT NULL)"
+                " ${DatabaseContract.FavoriteColumns.FAVORITE} TEXT NULL)"
     }
 
     override fun onCreate(db: SQLiteDatabase) {
